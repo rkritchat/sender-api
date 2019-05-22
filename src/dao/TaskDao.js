8f37229fcb4a093243e85a94b66505f415b7c5b2
rkritchat <rkritchat@gmail.com>
2019-05-22 23:12:48 +0700

    clean code

--- src/dao/TaskDao.js
+++ src/dao/TaskDao.js
@@ -1,23 +1,6 @@
 const _ = require('underscore')
 const mongoose = require('mongoose')
 
-// const taskInfo = [
-//     {
-//         username: 'rkrtichat', task: [
-//             { taskName: 'CGP', taskDesc: 'Clean code', taskProgress: '40' },
-//             { taskName: 'direcApprove', taskDesc: 'Create unit test for approve serice', taskProgress: '10' },
-//             { taskName: 'eCustom', taskDesc: 'Create e-service', taskProgress: '100' },
-//         ]
-//     }, {
-//         username: 'admin', task: [
-//             { taskName: 'aa', taskDesc: 'Test des of task aa', taskProgress: '10' },
-//             { taskName: 'bb', taskDesc: 'lorem', taskProgress: '5' }
-//         ]
-//     }
-// ]
-
-
-
 class TaskDao {
     constructor() {
         this.schema = mongoose.Schema({
